bd780f15cc554fdc701864529d61899067627830
Nikolay Novikov <AnourValar@gmail.com>
2025-08-17 14:24:10 +0300

    patch

--- src/Providers/LaravelFormServiceProvider.php
+++ src/Providers/LaravelFormServiceProvider.php
@@ -25,7 +25,7 @@ class LaravelFormServiceProvider extends ServiceProvider
     public function boot()
     {
         // config
-        $this->publishes([ __DIR__.'/../resources/config/form.php' => config_path('form.php')], 'config');
+        $this->publishes([__DIR__.'/../resources/config/form.php' => config_path('form.php')], 'config');
 
         // views
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'form');
