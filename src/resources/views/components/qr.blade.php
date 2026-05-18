72f88c9728200ffab8e7d180ac57fc02fb0b85f4
Nikolay Novikov <AnourValar@gmail.com>
2026-05-18 06:23:48 +0300

    patch

---
+++ src/resources/views/components/qr.blade.php
@@ -0,0 +1,24 @@
+@props(['value'])
+
+@if ($value)
+  <div class="alert alert-light alert-dismissible fade show alert-blocks fade-in" role="alert">
+    <div>
+        <div id="qrcode-{{ sha1($value) }}"></div>
+    </div>
+    <button
+      type="button"
+      class="close"
+      data-dismiss="alert"
+      aria-label="Close"
+    ><span aria-hidden="true">&times;</span></button>
+  </div>
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js"></script>
+  <script>
+  new QRCode(document.getElementById("qrcode-{{ sha1($value) }}"), {
+      text: @json($value, JSON_UNESCAPED_UNICODE),
+      width: 256,
+      height: 256
+  });
+  </script>
+@endif
