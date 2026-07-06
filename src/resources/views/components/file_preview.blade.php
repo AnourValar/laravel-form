461371afc818d37dc463cde1d5e114a2c2372f5e
Nikolay Novikov <AnourValar@gmail.com>
2026-07-06 11:05:10 +0300

    patch

--- src/resources/views/components/file_preview.blade.php
+++ src/resources/views/components/file_preview.blade.php
@@ -1,15 +1,16 @@
-@props(['url', 'width' => '30', 'height' => '30'])
+@props(['url', 'width' => '35', 'height' => '35'])
 
 @php
   $extension = mb_strtolower(pathinfo($url, PATHINFO_EXTENSION));
+  $extension = explode('?', $extension)[0];
 @endphp
 
 @if (in_array($extension, ['jpeg', 'jpg', 'png', 'svg', 'webp', 'gif', 'avif']))
   <img src="{{ $url }}" style="max-width: {{ $width }}px; max-height: {{ $height }}px;" {{ $attributes }} />
 @elseif (in_array($extension, ['mp4', 'mov', 'mkv', 'avi']))
- <span style="max-width: {{ $width }}px; max-height: {{ $height }}px;" {{ $attributes }}><i class="fa-solid fa-film"></i></span>
+ <span {{ $attributes }}><i class="fa-solid fa-film" style="font-size: {{ (int) ($height * 0.6) }}px;"></i></span>
 @elseif (in_array($extension, ['pdf']))
- <span style="max-width: {{ $width }}px; max-height: {{ $height }}px;" {{ $attributes }}><i class="fa-regular fa-file-pdf"></i></span>
+ <span {{ $attributes }}><i class="fa-regular fa-file-pdf" style="font-size: {{ (int) ($height * 0.6) }}px;"></i></span>
 @else
- <span style="max-width: {{ $width }}px; max-height: {{ $height }}px;" {{ $attributes }}><i class="fa-regular fa-file"></i></span>
+ <span {{ $attributes }}><i class="fa-regular fa-file" style="font-size: {{ (int) ($height * 0.6) }}px;"></i></span>
 @endif
